d8fdc26f529cf0b8b7ed6dae727bd86c631a5b02
Roman_Radetskiy <gressmc@gmail.com>
2018-10-22 18:52:06 +0400

    bla bla

--- src/main/java/com/gress/fortochka/services/KindergartenService.java
+++ src/main/java/com/gress/fortochka/services/KindergartenService.java
@@ -2,6 +2,7 @@ package com.gress.fortochka.services;
 
 import com.gress.fortochka.model.Kindergarten;
 import com.gress.fortochka.model.Kindergroup;
+import com.gress.fortochka.repositories.KinderGroupRepository;
 import com.gress.fortochka.repositories.KindergartenRepository;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.util.ArrayUtils;
@@ -17,9 +18,11 @@ import java.util.List;
 public class KindergartenService implements AbstractService<Kindergarten> {
 
     KindergartenRepository kindergartenRepository;
+    KinderGroupRepository kinderGroupRepository;
 
-    public KindergartenService(KindergartenRepository kindergartenRepository) {
+    public KindergartenService(KindergartenRepository kindergartenRepository, KinderGroupRepository kinderGroupRepository) {
         this.kindergartenRepository = kindergartenRepository;
+        this.kinderGroupRepository = kinderGroupRepository;
     }
 
     @Override
@@ -42,13 +45,15 @@ public class KindergartenService implements AbstractService<Kindergarten> {
         newKindergarten.setTitle(title);
 //        Arrays.stream(groups).forEach(Kindergroup::new);
         List<String> l = Arrays.asList(groups);
+        kindergartenRepository.save(newKindergarten);
         for (String t: l) {
             Kindergroup kindergroup = new Kindergroup();
             kindergroup.setTitle(t);
             kindergroup.setKindergarten(newKindergarten);
+            kinderGroupRepository.save(kindergroup);
         }
 
-        kindergartenRepository.save(newKindergarten);
+
         return newKindergarten;
     }
 }
